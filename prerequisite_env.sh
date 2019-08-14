cdbe7ef7d994cca5ef6e9a11a60afcf88a8d0f0b
root <root@k8s-master3.asia-east1-a.c.arctic-defender-237709.internal>
2019-08-14 06:45:08 +0000

    固定 kubenetes 安裝版號

--- prerequisite_env.sh
+++ prerequisite_env.sh
@@ -41,5 +41,5 @@ cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
-apt-get install -y kubelet kubeadm kubectl
+apt-get install -qy kubelet=1.14.3-00 kubeadm=1.14.3-00 kubectl=1.14.3-00
 apt-mark hold kubelet kubeadm kubectl
